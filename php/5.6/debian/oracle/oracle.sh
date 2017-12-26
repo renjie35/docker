6e998a16826b5a8efec446bc10e243e6d394afe4
renjie <renjie35@qq.com>
2017-12-26 13:46:14 +0800

    [dev]修正目录结构

--- php/5.6/debian/oracle/oracle.sh
+++ php/5.6/debian/oracle/oracle.sh
@@ -1,6 +1,6 @@
 # vi /etc/profile.d/oracle.sh
 export ORACLE_HOME=/usr/lib/oracle/12.2/client64
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib
-export TNS_ADMIN=$ORACLE_HOME/network/admin
+#export TNS_ADMIN=$ORACLE_HOME/network/admin
 export PATH=$PATH:$ORACLE_HOME/bin
-export NLS_LANG="SIMPLIFIED CHINESE_CHINA.ZHS16GBK"
+#export NLS_LANG="SIMPLIFIED CHINESE_CHINA.ZHS16GBK"
