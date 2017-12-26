96745813276e5e8b6b7138df7c1027e821f44f3a
renjie <renjie35@qq.com>
2017-12-26 15:24:03 +0800

    [dev]修正

--- php/5.6/debian/oracle/oracle.sh
+++
@@ -1,6 +0,0 @@
-# vi /etc/profile.d/oracle.sh
-export ORACLE_HOME=/usr/lib/oracle/12.2/client64
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib
-#export TNS_ADMIN=$ORACLE_HOME/network/admin
-export PATH=$PATH:$ORACLE_HOME/bin
-#export NLS_LANG="SIMPLIFIED CHINESE_CHINA.ZHS16GBK"
